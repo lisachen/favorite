ffca3350da91be578cf62bf4da8d45bc5b1e8324
lisa chen <conglvse@gmail.com>
2015-01-08 17:01:54 +0800

    20140923
    lisachen.sinapp.com

---
+++ js/myscript.js
@@ -0,0 +1,15 @@
+﻿$(function(){
+	//IE6提示
+	$('#ie6-close').click(function(){
+		$('#ie6-warning').hide();
+	})
+	
+	$('#mainNav li').click(function(){
+		a_s_r($(this),'active');
+	})
+})
+/*-------------自定义函数-------------------------*/
+function a_s_r(o,c){
+	o.addClass(c).siblings().removeClass(c);	
+}
+
